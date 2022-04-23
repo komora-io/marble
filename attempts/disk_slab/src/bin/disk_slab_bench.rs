5a884e0b31717685762c7340cf2607a44a44aa33
Tyler Neely <t@jujit.su>
2022-04-23 12:21:24 +0200

    Cleanup old attempts

--- attempts/disk_slab/src/bin/disk_slab_bench.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    let mut ds = disk_slab_bench::recover("disk_slab_bench").unwrap();
-}
--- attempts/log_apply/src/lib.rs
+++
@@ -1,6 +0,0 @@
-#[test]
-fn basic_functionality() {
-    let mut log = Log::new("basic_functionality").unwrap();
-
-    let mut log2 = log.new_shard();
-}
