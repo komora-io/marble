6669164cf009f607dc46ab41aca9e66fb645992e
Tyler Neely <t@jujit.su>
2022-07-03 15:50:25 +0200

    Update bench

--- examples/bench.rs
+++ examples/bench.rs
@@ -2,19 +2,21 @@ use std::sync::Arc;
 
 use marble::{Marble, ObjectId};
 
+const MUL: u64 = 1;
+const KEYSPACE: u64 = 64 * 1024;
+const BATCH_SZ: usize = 1024;
+const VALUE_LEN: usize = 4096;
+const OPS: usize = 2 * 1024;
+const BATCHES: usize = OPS / BATCH_SZ;
+
 fn rdtsc() -> u64 {
     unsafe { core::arch::x86_64::_rdtsc() }
 }
 
 fn run(marble: Arc<Marble>) {
-    const MUL: u64 = 1;
-    const MAX: u64 = 1024 * 1024;
-    const KEYSPACE: u64 = 64 * 1024;
-    const BATCH_SZ: u64 = 1024;
+    let v = vec![0xFA; VALUE_LEN];
 
-    let v = vec![0xFA; 4 * 1024];
-
-    for i in 0..(MAX / BATCH_SZ) {
+    for i in 0..BATCHES {
         let mut batch = std::collections::HashMap::new();
 
         for _ in 1..=BATCH_SZ {
@@ -37,6 +39,8 @@ fn main() {
 
     let mut threads = vec![];
 
+    let before = std::time::Instant::now();
+
     for _ in 0..concurrency {
         let marble = marble.clone();
         threads.push(std::thread::spawn(move || {
@@ -48,7 +52,15 @@ fn main() {
         thread.join().unwrap();
     }
 
-    dbg!(
-        u64::MAX - fault_injection::FAULT_INJECT_COUNTER.load(std::sync::atomic::Ordering::Acquire)
-    );
+    let total_ops = concurrency * BATCH_SZ * BATCHES;
+    let bytes_written = total_ops * VALUE_LEN;
+    let fault_injection_points =
+        u64::MAX - fault_injection::FAULT_INJECT_COUNTER.load(std::sync::atomic::Ordering::Acquire);
+    let elapsed = before.elapsed();
+    let writes_per_second = (total_ops as u128 * 1000) / elapsed.as_millis();
+
+    println!(
+        "wrote {} bytes in {:?} ({} writes per second), {} fault injection points",
+        bytes_written, elapsed, writes_per_second, fault_injection_points,
+    )
 }
