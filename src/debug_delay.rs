77e2d50c8f7264b0f6ca8daa7a5856df2fc6fb34
Tyler Neely <t@jujit.su>
2022-08-15 22:04:47 +0200

    Make debug delays feature-only

--- src/debug_delay.rs
+++ src/debug_delay.rs
@@ -1,16 +1,19 @@
-use std::thread;
-use std::time::Duration;
-
-use rand::{thread_rng, Rng};
-
 /// This function is useful for inducing random jitter into our atomic
 /// operations, shaking out more possible interleavings quickly. It gets
 /// fully eliminated by the compiler in non-test code.
 pub fn debug_delay() {
-    let mut rng = thread_rng();
+    #[cfg(feature = "runtime_verification")]
+    {
+        use std::thread;
+        use std::time::Duration;
 
-    match rng.gen_range(0..100) {
-        0..=90 => thread::yield_now(),
-        _ => thread::sleep(Duration::from_millis(5)),
+        use rand::{thread_rng, Rng};
+
+        let mut rng = thread_rng();
+
+        match rng.gen_range(0..100) {
+            0..=90 => thread::yield_now(),
+            _ => thread::sleep(Duration::from_millis(5)),
+        }
     }
 }
