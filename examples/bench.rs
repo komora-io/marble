d3ecd0fa9018fea1aef54c0ef8718f8d92deb281
Tyler Neely <t@jujit.su>
2022-08-20 12:57:27 +0200

    Update bench to be harder on the location table

--- examples/bench.rs
+++ examples/bench.rs
@@ -4,18 +4,18 @@ use rand::{thread_rng, Rng};
 
 use marble::Marble;
 
-const KEYSPACE: u64 = 64 * 1024;
-const BATCH_SZ: usize = 1024;
-const VALUE_LEN: usize = 4096 * 16;
-const OPS: usize = 24 * 1024 * 1024;
-const BATCHES: usize = OPS / BATCH_SZ;
+const KEYSPACE: u64 = 1024 * 1024;
+const BATCH_SZ: usize = 64 * 1024;
+const VALUE_LEN: usize = 64;
+const OPS_PER_THREAD: usize = 64 * 1024 * 1024;
+const BATCHES_PER_THREAD: usize = OPS_PER_THREAD / BATCH_SZ;
 
 fn run(marble: Arc<Marble>) {
     let v = vec![0xFA; VALUE_LEN];
 
     let mut rng = thread_rng();
 
-    for i in 0..BATCHES {
+    for i in 0..BATCHES_PER_THREAD {
         let mut batch = std::collections::HashMap::new();
 
         for _ in 0..BATCH_SZ {
@@ -23,14 +23,9 @@ fn run(marble: Arc<Marble>) {
             batch.insert(pid, Some(&v));
         }
 
+        println!("starting write batch");
         marble.write_batch(batch).unwrap();
-
-        if i % 16 == 0 {
-            let cleaned_up = marble.maintenance().unwrap();
-            if cleaned_up != 0 {
-                println!("defragmented {} objects", cleaned_up);
-            }
-        }
+        println!("wrote batch");
     }
 }
 
@@ -63,25 +58,34 @@ fn main() {
         )
     }
 
+    while threads.iter().any(|t| !t.is_finished()) {
+        let cleaned_up = marble.maintenance().unwrap();
+        if cleaned_up != 0 {
+            println!("defragmented {} objects", cleaned_up);
+        }
+    }
+
     for thread in threads {
         thread.join().unwrap();
     }
 
-    let total_ops = concurrency * BATCH_SZ * BATCHES;
+    let cleaned_up = marble.maintenance().unwrap();
+    if cleaned_up != 0 {
+        println!("defragmented {} objects", cleaned_up);
+    }
+
+    let total_ops = concurrency * BATCH_SZ * BATCHES_PER_THREAD;
     let bytes_written = total_ops * VALUE_LEN;
     let fault_injection_points =
         u64::MAX - fault_injection::FAULT_INJECT_COUNTER.load(std::sync::atomic::Ordering::Acquire);
     let elapsed = before.elapsed();
     let writes_per_second = (total_ops as u128 * 1000) / elapsed.as_millis();
-    let bytes_per_second = (bytes_written as u128 / 1000) / elapsed.as_millis();
+    let megabytes_per_second = (bytes_written as u128 / 1000) / elapsed.as_millis();
+    let megabytes_written = bytes_written / 1_000_000;
+    let kb_per_value = VALUE_LEN / 1_000;
 
     println!(
-        "wrote {} mb in {:?} with {} threads ({} writes per second, {} mb per second), {} fault injection points",
-        bytes_written / 1_000_000,
-        elapsed,
-        concurrency,
-        writes_per_second,
-        bytes_per_second,
-        fault_injection_points,
+        "wrote {megabytes_written} mb in {elapsed:?} with {concurrency} threads ({writes_per_second} objects per second \
+        of size {kb_per_value} kb each, {megabytes_per_second} mb per second), {fault_injection_points} fault injection points",
     )
 }
